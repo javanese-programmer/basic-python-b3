066cec361a4bcf4c3206d191226a269a3bc02666
unknown <tariq.fitria.aziz@mail.ugm.ac.id>
2021-01-31 12:53:14 +0700

    Final Project

--- Final-Project/final_project.py
+++ Final-Project/final_project.py
@@ -3,22 +3,42 @@
 #                 https://realpython.com/python-send-email/
 
 import smtplib, ssl
+import getpass
+
+print("\nAPLIKASI PENGIRIM EMAIL")
+
+def email_count():
+    jumlah_email = int(input("Jumlah Email Dikirim : "))
+    return jumlah_email
+def pesan():
+    f1 = open("pesan.txt", "r")
+    x = f1.readlines()
+    psn = ""
+    for i in range(0, len(x)):
+        psn = psn + x[i] + "\n"
+    return psn   
 
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
-sender_email = "tpython405@gmail.com"  # Enter your address
-receiver_email = "tpython405+person1@gmail.com"  # Enter receiver address
-password = input("Type your password and press enter: ")
-message = """\
-Subject: Hi there
 
-This message is sent from Python."""
+sender_email = input("Masukkan alamat pengirim : ")  # Enter your address
+print("User name : {}".format(sender_email))
+password = getpass.getpass(prompt="Masukkan password Anda!")
 
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+f2 = open("penerima.txt", "w")
+f2.write("")
+f2.close()
 
-f = open("receiver.txt", "w")
-f.write(receiver_email)
-f.close()
+for i in range(0,email_count()):
+    receiver_email = input("Masukkan alamat penerima : ")  # Enter receiver address
+    message = pesan()
+    
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+    
+    f2 = open("penerima.txt", "a")
+    f2.write(receiver_email)
+    f2.write("\n")
+    f2.close()
